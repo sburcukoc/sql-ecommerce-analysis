0004f7f83b68322d36a830f0ef11088030cfd12f
Burcu Koc <burcu.skoc@gmail.com>
2026-03-09 20:45:58 -0400

    add sample data

---
+++ data.sql
@@ -0,0 +1,8 @@
+INSERT INTO customers VALUES (1,'Burcu','USA');
+INSERT INTO customers VALUES (2,'Ali','Turkey');
+INSERT INTO customers VALUES (3,'John','USA');
+
+INSERT INTO orders VALUES (1,1,200);
+INSERT INTO orders VALUES (2,1,150);
+INSERT INTO orders VALUES (3,2,300);
+INSERT INTO orders VALUES (4,3,100);
